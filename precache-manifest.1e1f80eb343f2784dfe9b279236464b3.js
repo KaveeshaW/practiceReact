9758082ed5e7589d44c3762abcc1c68aa1726e0e
Kaveesha <weerasirikaveesha@gmail.com>
2020-05-11 16:04:22 -0500

    Updates

---
+++ precache-manifest.1e1f80eb343f2784dfe9b279236464b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e3a1abfd8e647bdbe4f9f7e9fdfa48",
+    "url": "/practiceReact/index.html"
+  },
+  {
+    "revision": "183a37575ee9b69148e3",
+    "url": "/practiceReact/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a740ed8f3265a1da9ecc",
+    "url": "/practiceReact/static/js/2.af71590a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practiceReact/static/js/2.af71590a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183a37575ee9b69148e3",
+    "url": "/practiceReact/static/js/main.d86c1bc4.chunk.js"
+  },
+  {
+    "revision": "5f471f954a85637232c4",
+    "url": "/practiceReact/static/js/runtime-main.dbfe8d15.js"
+  }
+]);
